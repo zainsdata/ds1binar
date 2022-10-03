c2b752a5a010091355535918730ba6d53fc17968
zains <112764037+zainsdata@users.noreply.github.com>
2022-10-03 16:43:43 +0700

    Delete goldcelens - ds1arjun - zains directory

--- goldcelens - ds1arjun - zains/sqlite.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1662625253896@@127.0.0.1@3306
-
-# Membuat table berisi Kolom ID, Tweet Sebelum dicleaning, Tweet Setelah dicleaning
-create table tweet
-(tweet_id INTEGER PRIMARY KEY AUTOINCREMENT, tweet_kotor text, tweet_bersih text);
-
-select * from tweet;
